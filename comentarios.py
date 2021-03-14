e6931d6792b2e385ed5e168ace761d467d5cb74c
Epetrel <miquelgabarro@gmail.com>
2021-03-14 19:30:55 +0100

    test

---
+++ comentarios.py
@@ -0,0 +1,11 @@
+# Esto me mostrará el texto por pantalla 
+print("Hola")
+print("Mundo")
+print("!!")
+
+"""
+print("Hola")
+print("Mundo")
+print("!!")
+"""
+print("Final comentario")
---
+++ variables-y-tipos/variable.py
@@ -0,0 +1,7 @@
+texto = "Master en Python"
+nombre = "John"
+apellido = "Alone"
+
+print(texto)
+print (f"{nombre} - {apellido}")
+print ("{} - {}".format(apellido,nombre))
